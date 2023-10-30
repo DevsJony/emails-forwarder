f1508b24ed39fd931e5a4823a34afd210f1623b0
EpicPlayerA10 <adasko.lesiak@gmail.com>
2023-10-30 19:28:34 +0100

    Initial commit

---
+++ src/email-listener.ts
@@ -0,0 +1,67 @@
+import {ImapFlow, ImapFlowOptions} from "imapflow";
+import {ParsedMail, simpleParser} from "mailparser";
+import {pino} from "pino";
+import EventEmitter from "events";
+import TypedEventEmitter, { EventMap } from "typed-emitter";
+
+type TypedEmitter<T extends EventMap> = TypedEventEmitter.default<T>;
+
+interface ExistsData {
+    path: string, // For example "INBOX"
+    count: number,
+    prevCount: number
+}
+
+type Events = {
+    onMailReceive: (mail: ParsedMail, mailId: number) => void
+}
+
+export class EmailListener extends (EventEmitter as new () => TypedEmitter<Events>) {
+    private client: ImapFlow;
+
+    constructor(imapOptions: ImapFlowOptions) {
+        super();
+
+        // Setup silent logger
+        let logger = pino();
+        logger.level = "silent";
+
+        this.client = new ImapFlow({
+            logger: logger,
+            ...imapOptions
+        });
+    }
+
+    private fromExistsDataToNewMails(data: ExistsData): number[] {
+        let newMails: number[] = [];
+        for (let mailId = data.prevCount + 1; mailId <= data.count; mailId++) {
+            newMails.push(mailId);
+        }
+
+        return newMails;
+    }
+
+    public async connect() {
+        console.log("Connecting to IMAP...");
+        await this.client.connect();
+
+        await this.client.getMailboxLock("INBOX");
+
+        console.log("Connected to IMAP!")
+
+        this.client.on("exists", async (data: ExistsData) => {
+            //console.log(`Message count in "${data.path}" is ${data.count}. Prev: ${data.prevCount}`);
+            //console.log("data", data);
+
+            let newMails = this.fromExistsDataToNewMails(data);
+
+            for (let mailId of newMails) {
+                let {meta, content} = await this.client.download(mailId.toString());
+
+                let parsedMail = await simpleParser(content);
+
+                this.emit("onMailReceive", parsedMail, mailId);
+            }
+        });
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import {EmailListener} from "./email-listener.js";
+import {ImapFlowOptions} from "imapflow";
+import fs from "fs";
+import {EmbedBuilder, WebhookClient} from "discord.js";
+import * as crypto from "crypto";
+
+interface EnvConfig {
+    instances: Array<{
+        mailAccount: ImapFlowOptions,
+        webhookUrl: string
+    }>
+}
+
+const envConfig = JSON.parse(fs.readFileSync("env-config.json", "utf-8")) as EnvConfig;
+
+for (let instance of envConfig.instances) {
+    console.log(`${instance.mailAccount.auth.user}: Preparing instance...`)
+    const emailListener = new EmailListener(instance.mailAccount);
+    const webhookClient = new WebhookClient({url: instance.webhookUrl})
+
+    emailListener.on("onMailReceive", async (mail, mailId) => {
+        console.log(`${instance.mailAccount.auth.user}: Processing ${mailId}`);
+        //console.log(JSON.stringify(mail));
+
+        // Avatar URL (Gravatar)
+        let lowerCaseMail = mail.from!.value[0]!.address!.trim().toLowerCase();
+        let emailHash = crypto.createHash("sha256").update(lowerCaseMail).digest("hex");
+        let avatarUrl = `https://gravatar.com/avatar/${emailHash}?d=404`;
+
+        // Prepare embed
+        let embed = new EmbedBuilder()
+            .setTitle(mail.subject!)
+            .setAuthor({name: mail.from!.text, iconURL: avatarUrl})
+            .setDescription(mail.text!)
+            .setFields({
+                name: "Liczba załączników",
+                value: mail.attachments.length.toString()
+            })
+            .setColor(0xd9ff00);
+
+        await webhookClient.send({ embeds: [embed] });
+    });
+
+    await emailListener.connect();
+
+    console.log(`${instance.mailAccount.auth.user}: Instance is running!`);
+}
